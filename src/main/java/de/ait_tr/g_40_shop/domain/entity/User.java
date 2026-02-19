5b1ce6d04271825ecc6b14f5adb72dba6136e119
Evgenygroup <mailfrom61@gmail.com>
2026-02-19 12:53:05 +0100

    adds email - sending and verification

--- src/main/java/de/ait_tr/g_40_shop/domain/entity/User.java
+++ src/main/java/de/ait_tr/g_40_shop/domain/entity/User.java
@@ -23,18 +23,6 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
@@ -42,6 +30,52 @@ public class User implements UserDetails {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "active")
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -63,17 +97,17 @@ public class User implements UserDetails {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles);
+        return active == user.active && Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles) && Objects.equals(email, user.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password, roles);
+        return Objects.hash(id, username, password, roles, email, active);
     }
 
     @Override
     public String toString() {
-        return String.format("User: id - %d, username - %s, role - %s",
-                id, username, roles == null ? "empty" : roles);
+        return String.format("User: id - %d, username - %s, email - %s, active - %s, role - %s",
+                id, username, email, active ? "yes" : "no", roles == null ? "empty" : roles);
     }
 }
